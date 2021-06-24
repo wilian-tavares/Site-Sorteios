dc351a1576c14f96776e636e5239c2a3aedff5fd
wilian-tavares <wilian.tavares@souunit.com.br>
2021-06-24 11:54:07 -0300

    site sorteios

---
+++ javascript.js
@@ -0,0 +1,141 @@
+
+
+/* Função Para Ordenar */
+function ordenador(a, b) {
+    if (a == b) return 0;
+    if (a < b) return -1;
+    if (a > b) return 1;
+}
+function compararNumeros(a, b) {
+    return a - b;
+}
+
+
+function sorteioMega(valor) {
+    var numerosSorteados = [];
+    var min = 1;
+    var max = 60;
+    while (numerosSorteados.length < 6) {
+
+
+        var dezena = (Math.floor(Math.random() * (max - min + 1)) + min);
+        dezena = dezena.toFixed();
+
+        if (numerosSorteados.includes(dezena)) {
+        }
+        else {
+            numerosSorteados.push(dezena);
+        }
+    }
+
+    numerosSorteados.sort(compararNumeros);
+    document.getElementById("resultado-mega").value = numerosSorteados;
+}
+
+
+function sorteioLotofacil(valor) {
+    var numerosSorteados = [];
+    var min = 1;
+    var max = 25;
+
+    while (numerosSorteados.length < 15) {
+
+        var dezena = (Math.floor(Math.random() * (max - min + 1)) + min);
+        dezena = dezena.toFixed();
+
+        if (numerosSorteados.includes(dezena)) {
+        }
+
+        else {
+            numerosSorteados.push(dezena);
+        }
+    }
+    numerosSorteados.sort(compararNumeros);
+
+    /*  codigo CSS*/
+    document.getElementById('resultado-lotofacil').style.color = "red";
+    document.getElementById('resultado-lotofacil').style.fontSize = "1.5rem";
+    /*  codigo CSS*/
+
+    document.getElementById('resultado-lotofacil').value = numerosSorteados;
+}
+
+function sorteioQuina(valor) {
+    var numerosSorteados = [];
+    var min = 1;
+    var max = 80;
+    while (numerosSorteados.length < 5) {
+        dezena = (Math.floor(Math.random() * (max - min + 1)) + min);
+        dezena = dezena.toFixed();
+
+        if (numerosSorteados.includes(dezena)) {
+
+        }
+        else {
+            numerosSorteados.push(dezena);
+        }
+    }
+    numerosSorteados.sort(compararNumeros);
+    document.getElementById('resultado-quina').value = numerosSorteados;
+}
+
+function sorteioMilhar(valor) {
+    min = 0000;
+    max = 9999;
+
+
+    milhar = (Math.floor(Math.random() * (max - min + 1)) + min);
+
+    document.getElementById('resultado-milhar').value = milhar;
+}
+
+function sorteioCentena(valor) {
+    min = 000;
+    max = 999;
+
+    centena = (Math.floor(Math.random() * (max - min + 1)) + min);
+
+    document.getElementById('resultado-centena').value = centena;
+}
+
+function sorteioDdz(valor) {
+    numerosSorteados = []
+    min = 01;
+    max = 99;
+
+    while (numerosSorteados.length < 2) {
+        ddz = (Math.floor(Math.random() * (max - min + 1)) + min);
+
+        if (numerosSorteados.includes(ddz)) {
+
+        }
+        else {
+            numerosSorteados.push(ddz);
+        }
+
+    }
+    numerosSorteados.sort(compararNumeros);
+    document.getElementById('resultado-ddz').value = numerosSorteados;
+}
+
+function sorteioTdz(valor) {
+    numerosSorteados = []
+    min = 01;
+    max = 99;
+
+    while (numerosSorteados.length < 3) {
+        tdz = (Math.floor(Math.random() * (max - min + 1)) + min);
+        if (numerosSorteados.includes(tdz)) {
+
+        }
+        else {
+            numerosSorteados.push(tdz);
+        }
+
+
+    }
+    numerosSorteados.sort(compararNumeros);
+    document.getElementById('resultado-tdz').value = numerosSorteados;
+}
+
+
